ed2913981b918694674e93c4004c1db5b46de481
Xx_arteeem_xX <artem.firsov.qwe@gmail.com>
2025-04-22 14:22:11 +0700

    make example app

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import express from "express"
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+const data = [
+    {
+        "name": "test1",
+        "desc": "lorem1"
+    },
+    {
+        "name": "test2",
+        "desc": "lorem2"
+    },
+    {
+        "name": "test3",
+        "desc": "lorem3"
+    },
+]
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.json(data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
