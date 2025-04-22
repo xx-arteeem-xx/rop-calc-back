345e89b22fe9a09ceff628832149e798fbf10503
Xx_arteeem_xX <artem.firsov.qwe@gmail.com>
2025-04-22 14:37:31 +0700

    timeweb deploy test

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const data = [
+  {
+    "name": "123",
+    "desc": "test"
+  },
+  {
+    "name": "456",
+    "desc": "test"
+  },
+  {
+    "name": "789",
+    "desc": "test"
+  }
+] 
+
 app.get('/', (req, res) => {
   res.send('Timeweb Cloud + Express = ️ ❤️')
 })
