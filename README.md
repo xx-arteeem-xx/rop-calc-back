1217aef871ed6eca767da11170f0e0b5c4aceab3
Xx_arteeem_xX <artem.firsov.qwe@gmail.com>
2025-04-22 17:35:56 +0700

    add a db connection

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 
 # Express
 
-Пример приложения [Express](https://expressjs.com/), которое можно развернуть в **Timeweb Cloud Apps** без настройки.
-
-:tada: [Демо](https://timeweb-cloud-app-example-express-e1cb.twc1.net)
-
-:rocket: [Создать свой Apps](https://timeweb.cloud/my/apps/create)
-
-:books: [Документация Timeweb Cloud Apps](https://timeweb.cloud/docs/apps)
+#### Приложение [Express](https://expressjs.com/)
 
 ## <a name="dev"></a>Локальный запуск проекта
 
--- index.js
+++ index.js
@@ -1,18 +1,42 @@
+// _____________________ ИМПОРТ БИБЛИОТЕК ______________________________________
 const express = require('express');
 require('dotenv').config();
+const pgp = require("pg-promise")();
+
+
+// ______________ НАСТРОЙКА ПАРАМЕТРОВ ПРИЛОЖЕНИЯ ______________________________
+const dbPath = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const db = pgp(dbPath);
 const app = express();
 const port = 3000;
 
-const data = {
-    "DB_NAME": process.env.DB_NAME || "testDB",
-    "DB_HOST": process.env.DB_HOST || "localhost",
-    "DB_PORT": process.env.DB_PORT || "5432",
-};
 
-app.get('/', (req, res) => {
-    res.json(data);
+// ________________________ МЕТОДЫ ____________________________________________
+
+// МЕТОД 1. При переходе на страницу "/api/getusers" получение списка всех пользователей.
+app.get('/api/getusers/', (req, res) => {
+    db.any("SELECT * FROM users")
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch(function (error) {
+            res.json({ error });
+        });
 });
 
+// МЕТОД 2. При переходе на страницу "/api/getusers/${id}" получение пользователя по ID.
+app.get('/api/getusers/:id', (req, res) => {
+    db.one(`SELECT * FROM users WHERE id=${req.params.id}`)
+        .then(function (data) {
+            res.json(data);
+        })
+        .catch(function (error) {
+            res.send(`Ошибка: ${error}`);
+        });
+});
+
+
+// _____________________ ЗАПУСК ПРИЛОЖЕНИЯ _____________________________________
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
