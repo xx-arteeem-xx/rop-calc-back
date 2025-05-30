7ea719fe39039ad2e15853083689913322a7ac7a
Xx_arteeem_xX <artem.firsov.qwe@gmail.com>
2025-05-30 12:33:27 +0700

    make code more useful and readable

--- index.js
+++ index.js
@@ -15,29 +15,69 @@ const port = 3000;
 
 // ________________________ МЕТОДЫ ____________________________________________
 
-// || МЕТОД 1. При переходе на страницу "/api/calc/income/" Подсчет количества денег, которые приносят студенты. ||
-// || Необходимо передать 2 массива: cash - в котором указать стоимость курса, и массив students - в нем количество учащихся на каждом курсе||
+// || МЕТОД 1. При переходе на страницу "/api/calc/income/" Подсчет количества доходов направления ||
+// || Пример тела запроса: 
+// {
+//     "data": [
+//         {
+//             "cash": [144718, 144718, 144718, 144718],
+//             "students": [0, 0, 14, 21]
+//         },
+//         {
+//             "cash": [136200, 136200, 135900, 141600],
+//             "students": [18, 65, 41, 12]
+//         }
+//     ]
+// }
+// 
+// || Пример ответа: 
+// {
+//     "budget": 5065130,
+//     "commerce": 18575700,
+//     "sum": 23640830
+// }
 app.post('/api/calc/income/', (req, res) => {
-    if (!req.body) {
-        return res.sendStatus(400);
-    };
     try {
-        let result = 0;
-        if (req.body.cash.length != req.body.students.length){
-            throw new Error("Количество элементов этих массивов должно совпадать!");
+        // _______________ ЗАДАЕМ ПЕРЕМЕННЫЕ __________________________________
+        let budget = 0;
+        let commerce = 0;
+        let sum = 0;
+        let cash1 = req.body.data[0].cash;
+        let cash2 = req.body.data[1].cash;
+        let students1 = req.body.data[0].students;
+        let students2 = req.body.data[1].students;
+
+        // _______________ ПРОВЕРЯЕМ НА ОШИБКИ В ДАННЫХ ________________________
+        if ((cash1.length != students1.length) || (cash2.length != students2.length)){
+            throw new Error("Count of students and cash must be equal!");
         }
-        for (let i = 0; i < req.body.cash.length; i++) {
-            result += req.body.cash[i] * req.body.students[i]
+
+        // _______________ ПРОВОДИМ РАССЧЕТЫ ___________________________________
+        for (let i = 0; i < cash1.length; i++) {
+            budget += cash1[i] * students1[i]
         };
-        res.status(200).json(result);
+        for (let i = 0; i < cash2.length; i++) {
+            commerce += cash2[i] * students2[i]
+        };
+        sum = budget + commerce;
+
+        // _______________ ОТПРАВЛЯЕМ ДАННЫЕ ____________________________________
+        res.status(200).json({
+            budget,
+            commerce,
+            sum
+        });
         logger.info({
             "path": req.path,
             "ip": req.ip
         });
     } catch (error) {
-        res.status(500).json({ error });
+        // _______________ ЕСЛИ НАШЛИ ОШИБКУ ____________________________________
+        res.status(400).json({
+            "error": error.message
+        });
         logger.error({
-            "error": error.name,
+            "error": error.message,
             "path": req.path,
             "ip": req.ip
         });
